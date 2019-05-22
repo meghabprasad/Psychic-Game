6d2f3c3ff0b3a273c006a8c91e15a13162d2b077
Megha Prasad Bindiganavale <Megha@calvisitor-10-105-192-128.calvisitor.1918.berkeley.edu>
2019-05-22 12:41:41 -0700

    create variables in js file

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -12,3 +12,10 @@
     //push the user-text to guessesMade array;
     //display everything to the screen
 //}
+
+var letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var correctLetter;
+var wins = 0;
+var loses = 0;
+var guessesLeft = 0;
+var guessesMade = [];
