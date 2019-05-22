fbedd5f19ed14a6ea080f1e3a2ea13a1d8015f8a
Megha Prasad Bindiganavale <Megha@calvisitor-10-105-192-128.calvisitor.1918.berkeley.edu>
2019-05-22 12:37:35 -0700

    create pseudocode in js file

---
+++ assests/javascript/game.js
@@ -0,0 +1,14 @@
+// create an array called letterOptions of possible letters
+// create var correctLetter to store the randomly picked letter 
+// create counter for wins and set to 0 
+// create counter for loses and set to 0
+// create counter for guesses left and set to 5;
+// create empty array for guessesMade;
+// listen to the key pressed by user onkeyup {
+    //store event.key to a var user-text
+    //check to see if user-text is equal to any of the elements in the letterOptions array
+    //if it is wins++
+    //if it is not loses++
+    //push the user-text to guessesMade array;
+    //display everything to the screen
+//}
